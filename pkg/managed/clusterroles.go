b4c3058920ba8edf2b3d884a449af9243286a175
Robert Donat <robert.donat@oracle.com>
2020-09-03 21:24:38 -0400

    Update verrazzano-system clusterrole to fix weblogic-operator permission error (#109)
    * add patch permission to verrazzano-system clusterrolebinding
    
    * add patch permission to verrazzano-system rolebinding

--- pkg/managed/clusterroles.go
+++ pkg/managed/clusterroles.go
@@ -211,8 +211,13 @@ func newClusterRoles(binding *v1beta1v8o.VerrazzanoBinding, clusterName string) 
 			},
 			{
 				APIGroups: []string{"rbac.authorization.k8s.io"},
-				Resources: []string{"clusterroles", "clusterrolebindings", "roles", "rolebindings"},
+				Resources: []string{"clusterroles", "roles"},
 				Verbs:     []string{"get", "list", "watch", "create", "update", "delete"},
+			},
+			{
+				APIGroups: []string{"rbac.authorization.k8s.io"},
+				Resources: []string{"clusterrolebindings", "rolebindings"},
+				Verbs:     []string{"get", "list", "watch", "create", "update", "delete", "patch"},
 			},
 			{
 				APIGroups: []string{"weblogic.oracle"},
