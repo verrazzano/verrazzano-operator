4c03926f55f7111bb5862b666987fecf787bcaff
Paul Mackin <paul.mackin@oracle.com>
2020-09-23 19:49:15 -0400

    checkpoint

--- pkg/monitoring/daemonsets_test.go
+++ pkg/monitoring/daemonsets_test.go
@@ -28,6 +28,12 @@ func TestCreateFilebeatDaemonSet(t *testing.T) {
 		case constants.FilebeatName:
 			validateFilebeatDaemonset(assert, *v, clusterName)
 			break
+		case constants.JournalbeatName:
+			validateJournalbeatDaemonset(assert, *v, clusterName)
+			break
+		case constants.NodeExporterName:
+			validateNodeExporterDaemonSet(assert, *v, clusterName)
+			break
 		}
 		delete(nameMap,v.Name)
 	}
@@ -139,7 +145,7 @@ func validateFilebeatDaemonset(assert *assert.Assertions, v appsv1.DaemonSet, cl
 }
 
 func validateJournalbeatDaemonset(assert *assert.Assertions, v appsv1.DaemonSet, clusterName string) {
-	labels := GetFilebeatLabels(clusterName)
+	labels := GetJournalbeatLabels(clusterName)
 
 	assert.Equal(constants.LoggingNamespace, v.Namespace)
 	assert.Equal(labels, v.Labels)
@@ -176,7 +182,7 @@ func validateJournalbeatDaemonset(assert *assert.Assertions, v appsv1.DaemonSet,
 	assert.Len( v.Spec.Template.Spec.Containers[0].Args,3,
 		"Spec.Template.Spec.Containers[0].Args has the wrong number of args)" )
 	assert.Equal("-c", v.Spec.Template.Spec.Containers[0].Args[0])
-	assert.Equal("/etc/filebeat/journalbeat.yml", v.Spec.Template.Spec.Containers[0].Args[1])
+	assert.Equal("/etc/journalbeat/journalbeat.yml", v.Spec.Template.Spec.Containers[0].Args[1])
 	assert.Equal("-e", v.Spec.Template.Spec.Containers[0].Args[2])
 	validateResSize(assert, v.Spec.Template.Spec.Containers[0].Resources.Limits, "memory", "800Mi",
 		"Spec.Template.Spec.Containers[0].Resources.Limits")
@@ -236,7 +242,7 @@ func validateJournalbeatDaemonset(assert *assert.Assertions, v appsv1.DaemonSet,
 
 	assert.Equal("etc-machine-id", v.Spec.Template.Spec.Containers[0].VolumeMounts[4].Name)
 	assert.True(v.Spec.Template.Spec.Containers[0].VolumeMounts[4].ReadOnly)
-	assert.Equal("etc-machine-id", v.Spec.Template.Spec.Containers[0].VolumeMounts[4].MountPath)
+	assert.Equal("/etc/machine-id", v.Spec.Template.Spec.Containers[0].VolumeMounts[4].MountPath)
 
 	assert.Equal(corev1.PullIfNotPresent, v.Spec.Template.Spec.Containers[0].ImagePullPolicy)
 
@@ -244,8 +250,92 @@ func validateJournalbeatDaemonset(assert *assert.Assertions, v appsv1.DaemonSet,
 	assert.Zero(*v.Spec.Template.Spec.Containers[0].SecurityContext.RunAsUser)
 	assert.Nil(v.Spec.Template.Spec.Containers[0].SecurityContext.ProcMount)
 
-	assert.Equal(30, *v.Spec.Template.Spec.TerminationGracePeriodSeconds)
+	assert.Equal(int64(30), *v.Spec.Template.Spec.TerminationGracePeriodSeconds)
 	assert.Equal(v.Name, v.Spec.Template.Spec.ServiceAccountName)
+}
+
+func validateNodeExporterDaemonSet(assert *assert.Assertions, v appsv1.DaemonSet, clusterName string) {
+	labels := GetNodeExporterLabels(clusterName)
+
+	applabel := map[string]string{
+		constants.ServiceAppLabel: constants.NodeExporterName,
+	}
+	assert.Equal(constants.MonitoringNamespace, v.Namespace)
+	assert.Equal(labels, v.Labels)
+	assert.Equal(applabel, v.Spec.Selector.MatchLabels)
+	assert.Equal("RollingUpdate", string(v.Spec.UpdateStrategy.Type))
+	assert.Equal(labels, v.Spec.Template.Labels)
+
+	// Annotations
+	assert.Equal("true", v.Spec.Template.Annotations["prometheus.io/scrape"])
+	assert.Equal("9100", v.Spec.Template.Annotations["prometheus.io/port"])
+
+	// Volumes
+	assert.Lenf(v.Spec.Template.Spec.Volumes,3, "Spec.Template.Spec.Volumes has wrong number of items")
+
+	assert.Equal("proc", v.Spec.Template.Spec.Volumes[0].Name)
+	assert.Equal("/proc", v.Spec.Template.Spec.Volumes[0].VolumeSource.HostPath.Path)
+
+	assert.Equal("sys", v.Spec.Template.Spec.Volumes[1].Name)
+	assert.Equal("/sys", v.Spec.Template.Spec.Volumes[1].VolumeSource.HostPath.Path)
+
+	assert.Equal("root", v.Spec.Template.Spec.Volumes[2].Name)
+	assert.Equal("/", v.Spec.Template.Spec.Volumes[2].VolumeSource.HostPath.Path)
+
+	// Containers
+	assert.Lenf(v.Spec.Template.Spec.Containers,1, "Spec.Template.Spec.Containers has wrong number of items")
+	assert.Equal(v.Name, v.Spec.Template.Spec.Containers[0].Name)
+	assert.Equal(util.GetNodeExporterImage(), v.Spec.Template.Spec.Containers[0].Image)
+	assert.Len( v.Spec.Template.Spec.Containers[0].Args,6,
+		"Spec.Template.Spec.Containers[0].Args has the wrong number of args)" )
+	assert.Equal("--web.listen-address=0.0.0.0:9100", v.Spec.Template.Spec.Containers[0].Args[0])
+	assert.Equal("--path.procfs=/host/proc", v.Spec.Template.Spec.Containers[0].Args[1])
+	assert.Equal("--path.sysfs=/host/sys", v.Spec.Template.Spec.Containers[0].Args[2])
+	assert.Equal("--path.rootfs=/host/root", v.Spec.Template.Spec.Containers[0].Args[3])
+	assert.Equal("--collector.filesystem.ignored-mount-points=^/(dev|proc|sys|var/lib/docker/.+)($|/)",
+		v.Spec.Template.Spec.Containers[0].Args[4])
+	assert.Equal("--collector.filesystem.ignored-fs-types=^(autofs|binfmt_misc|cgroup|configfs|debugfs|devpts|devtmpfs|fusectl|hugetlbfs|mqueue|overlay|proc|procfs|pstore|rpc_pipefs|securityfs|sysfs|tracefs)$",
+		v.Spec.Template.Spec.Containers[0].Args[5])
+
+	// Ports
+	assert.Equal("metrics", v.Spec.Template.Spec.Containers[0].Ports[0].Name)
+	assert.Equal(int32(9100), v.Spec.Template.Spec.Containers[0].Ports[0].ContainerPort)
+	assert.Equal(int32(9100), v.Spec.Template.Spec.Containers[0].Ports[0].HostPort)
+	assert.Equal("TCP", string(v.Spec.Template.Spec.Containers[0].Ports[0].Protocol))
+
+	validateResSize(assert, v.Spec.Template.Spec.Containers[0].Resources.Limits, "cpu", "250m",
+		"Spec.Template.Spec.Containers[0].Resources.Limits")
+	validateResSize(assert, v.Spec.Template.Spec.Containers[0].Resources.Limits, "memory", "180Mi",
+		"Spec.Template.Spec.Containers[0].Resources.Limits")
+	validateResSize(assert, v.Spec.Template.Spec.Containers[0].Resources.Requests, "cpu", "102m",
+		"Spec.Template.Spec.Containers[0].Resources.Limits")
+	validateResSize(assert, v.Spec.Template.Spec.Containers[0].Resources.Requests, "memory", "180Mi",
+		"Spec.Template.Spec.Containers[0].Resources.Limits")
+
+	// Volume mounts
+	assert.Equal("proc", v.Spec.Template.Spec.Containers[0].VolumeMounts[0].Name)
+	assert.True(v.Spec.Template.Spec.Containers[0].VolumeMounts[0].ReadOnly)
+	assert.Equal("/host/proc", v.Spec.Template.Spec.Containers[0].VolumeMounts[0].MountPath)
+
+	assert.Equal("sys", v.Spec.Template.Spec.Containers[0].VolumeMounts[1].Name)
+	assert.True(v.Spec.Template.Spec.Containers[0].VolumeMounts[1].ReadOnly)
+	assert.Equal("/host/sys", v.Spec.Template.Spec.Containers[0].VolumeMounts[1].MountPath)
+
+	assert.Equal("root", v.Spec.Template.Spec.Containers[0].VolumeMounts[2].Name)
+	assert.True(v.Spec.Template.Spec.Containers[0].VolumeMounts[2].ReadOnly)
+	assert.Equal("/host/root", v.Spec.Template.Spec.Containers[0].VolumeMounts[2].MountPath)
+
+	assert.Equal(corev1.PullIfNotPresent, v.Spec.Template.Spec.Containers[0].ImagePullPolicy)
+
+	assert.Equal("Always", string(v.Spec.Template.Spec.RestartPolicy))
+	assert.Equal("ClusterFirst", string(v.Spec.Template.Spec.DNSPolicy))
+	assert.Equal(v.Name, v.Spec.Template.Spec.ServiceAccountName)
+	assert.True(v.Spec.Template.Spec.HostNetwork)
+	assert.True(v.Spec.Template.Spec.HostPID)
+	assert.Equal("linux", v.Spec.Template.Spec.NodeSelector["beta.kubernetes.io/os"])
+	assert.Equal(int64(65534), *v.Spec.Template.Spec.SecurityContext.RunAsUser)
+	assert.Equal("NoSchedule", string(v.Spec.Template.Spec.Tolerations[0].Effect))
+	assert.Equal("Exists", string(v.Spec.Template.Spec.Tolerations[0].Operator))
 }
 
 
