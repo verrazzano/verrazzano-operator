a61fc3b30901b3d31a5b293d43263dfa6e3f2245
Michael Gianatassio <31552226+mgianatagh@users.noreply.github.com>
2020-09-23 17:24:12 -0400

    Add unit tests for pkg/monitoring/clusterrolebinding (#135)

--- pkg/monitoring/clusterroles_test.go
+++ pkg/monitoring/clusterroles_test.go
@@ -11,10 +11,11 @@ import (
 )
 
 func TestGetSystemClusterRoles(t *testing.T) {
+	assert := assert.New(t)
 	clusterName := "test-cluster"
 	clusterRoles := GetSystemClusterRoles(clusterName)
-	assert.NotNil(t, clusterRoles, "Expected the response to not be nil")
-	assert.Len(t, clusterRoles, 3, "Expected three ClusterRoles to be returned")
+	assert.NotNil(clusterRoles, "Expected the response to not be nil")
+	assert.Len(clusterRoles, 3, "Expected three ClusterRoles to be returned")
 
 	// Validate that all the expected ClusterRoles were returned
 	fileBeatFound := false
@@ -27,31 +28,31 @@ func TestGetSystemClusterRoles(t *testing.T) {
 		switch clusterRole.Name {
 		case constants.FilebeatName:
 			fileBeatFound = true
-			assert.Lenf(t, labels, 3, "Expected three labels in ClusterRole %s", clusterRole.Name)
-			assert.Equalf(t, constants.FilebeatName, labels[constants.K8SAppLabel], "Expected label %s to be %s", constants.K8SAppLabel, constants.FilebeatName)
+			assert.Lenf(labels, 3, "Expected three labels in ClusterRole %s", clusterRole.Name)
+			assert.Equalf(constants.FilebeatName, labels[constants.K8SAppLabel], "Expected label %s to be %s", constants.K8SAppLabel, constants.FilebeatName)
 
 		case constants.JournalbeatName:
 			journalBeatFound = true
-			assert.Lenf(t, labels, 3, "Expected three labels in ClusterRole %s", clusterRole.Name)
-			assert.Equalf(t, constants.JournalbeatName, labels[constants.K8SAppLabel], "Expected label %s to be %s", constants.K8SAppLabel, constants.JournalbeatName)
+			assert.Lenf(labels, 3, "Expected three labels in ClusterRole %s", clusterRole.Name)
+			assert.Equalf(constants.JournalbeatName, labels[constants.K8SAppLabel], "Expected label %s to be %s", constants.K8SAppLabel, constants.JournalbeatName)
 
 		case constants.NodeExporterName:
 			nodeExporterFound = true
-			assert.Lenf(t, labels, 3, "Expected three labels in ClusterRole %s", clusterRole.Name)
-			assert.Equalf(t, constants.NodeExporterName, labels[constants.ServiceAppLabel], "Expected label %s to be %s", constants.ServiceAppLabel, constants.NodeExporterName)
+			assert.Lenf(labels, 3, "Expected three labels in ClusterRole %s", clusterRole.Name)
+			assert.Equalf(constants.NodeExporterName, labels[constants.ServiceAppLabel], "Expected label %s to be %s", constants.ServiceAppLabel, constants.NodeExporterName)
 
 			policyRules := clusterRole.Rules
-			assert.Lenf(t, policyRules, 1, "Expected one policy rule created for ClusterRoles %s", clusterRole.Name)
+			assert.Lenf(policyRules, 1, "Expected one policy rule created for ClusterRoles %s", clusterRole.Name)
 			policyRule := policyRules[0]
-			assert.Lenf(t, policyRule.APIGroups, 1, "Expected one APIGroup entry for ClusterRole %s and found %q", clusterRole.Name, policyRule.APIGroups)
-			assert.Contains(t, policyRule.APIGroups, "extensions")
-			assert.Lenf(t, policyRule.Resources, 1, "Expected one Resources entry for ClusterRole %s and found %q", clusterRole.Name, policyRule.Resources)
-			assert.Contains(t, policyRule.Resources, "podsecuritypolicies")
-			assert.Lenf(t, policyRule.Verbs, 1, "Expected on Verbs entry for ClusterRole %s and found %q", clusterRole.Name, policyRule.Resources)
-			assert.Contains(t, policyRule.Verbs, "use")
-			assert.Lenf(t, policyRule.ResourceNames, 1, "Expected one ResourceNames entry for ClusterRole %s and found %q", clusterRole.Name, policyRule.ResourceNames)
-			assert.Contains(t, policyRule.ResourceNames, "system-node-exporter")
-			assert.Lenf(t, policyRule.NonResourceURLs, 0, "Expected no NonResourceURLs entry for ClusterRole %s and found %q", clusterRole.Name, policyRule.NonResourceURLs)
+			assert.Lenf(policyRule.APIGroups, 1, "Expected one APIGroup entry for ClusterRole %s and found %q", clusterRole.Name, policyRule.APIGroups)
+			assert.Contains(policyRule.APIGroups, "extensions")
+			assert.Lenf(policyRule.Resources, 1, "Expected one Resources entry for ClusterRole %s and found %q", clusterRole.Name, policyRule.Resources)
+			assert.Contains(policyRule.Resources, "podsecuritypolicies")
+			assert.Lenf(policyRule.Verbs, 1, "Expected on Verbs entry for ClusterRole %s and found %q", clusterRole.Name, policyRule.Resources)
+			assert.Contains(policyRule.Verbs, "use")
+			assert.Lenf(policyRule.ResourceNames, 1, "Expected one ResourceNames entry for ClusterRole %s and found %q", clusterRole.Name, policyRule.ResourceNames)
+			assert.Contains(policyRule.ResourceNames, "system-node-exporter")
+			assert.Lenf(policyRule.NonResourceURLs, 0, "Expected no NonResourceURLs entry for ClusterRole %s and found %q", clusterRole.Name, policyRule.NonResourceURLs)
 
 		default:
 			unexpectedNamesFound = true
@@ -59,31 +60,31 @@ func TestGetSystemClusterRoles(t *testing.T) {
 		}
 
 		// Common label checks
-		assert.Equalf(t, labels[constants.VerrazzanoBinding], constants.VmiSystemBindingName, "Expected label %s to be %s for ClusterRole %s", constants.VerrazzanoBinding, constants.VmiSystemBindingName, clusterRole.Name)
-		assert.Equalf(t, labels[constants.VerrazzanoCluster], clusterName, "Expected label %s to be %s for ClusterRole %s", constants.VerrazzanoCluster, clusterName, clusterRole.Name)
+		assert.Equalf(labels[constants.VerrazzanoBinding], constants.VmiSystemBindingName, "Expected label %s to be %s for ClusterRole %s", constants.VerrazzanoBinding, constants.VmiSystemBindingName, clusterRole.Name)
+		assert.Equalf(labels[constants.VerrazzanoCluster], clusterName, "Expected label %s to be %s for ClusterRole %s", constants.VerrazzanoCluster, clusterName, clusterRole.Name)
 
 		// Policy checks that are common to FileBeat and JournalBeat
 		if clusterRole.Name == constants.FilebeatName || clusterRole.Name == constants.JournalbeatName {
 			policyRules := clusterRole.Rules
-			assert.Lenf(t, policyRules, 1, "Expected one policy rule created for ClusterRoles %s", clusterRole.Name)
+			assert.Lenf(policyRules, 1, "Expected one policy rule created for ClusterRoles %s", clusterRole.Name)
 			policyRule := policyRules[0]
-			assert.Lenf(t, policyRule.APIGroups, 1, "Expected one APIGroup entry for ClusterRole %s and found %q", clusterRole.Name, policyRule.APIGroups)
-			assert.Contains(t, policyRule.APIGroups, "")
-			assert.Lenf(t, policyRule.Resources, 4, "Expected four Resources entry for ClusterRole %s and found %q", clusterRole.Name, policyRule.Resources)
-			assert.Contains(t, policyRule.Resources, "pods")
-			assert.Contains(t, policyRule.Resources, "namespaces")
-			assert.Contains(t, policyRule.Resources, "services")
-			assert.Contains(t, policyRule.Resources, "daemonsets")
-			assert.Lenf(t, policyRule.Verbs, 3, "Expected three Verbs entry for ClusterRole %s and found %q", clusterRole.Name, policyRule.Resources)
-			assert.Contains(t, policyRule.Verbs, "get")
-			assert.Contains(t, policyRule.Verbs, "watch")
-			assert.Contains(t, policyRule.Verbs, "list")
-			assert.Lenf(t, policyRule.ResourceNames, 0, "Expected no ResourceNames entry for ClusterRole %s and found %q", clusterRole.Name, policyRule.ResourceNames)
-			assert.Lenf(t, policyRule.NonResourceURLs, 0, "Expected no NonResourceURLs entry for ClusterRole %s and found %q", clusterRole.Name, policyRule.NonResourceURLs)
+			assert.Lenf(policyRule.APIGroups, 1, "Expected one APIGroup entry for ClusterRole %s and found %q", clusterRole.Name, policyRule.APIGroups)
+			assert.Contains(policyRule.APIGroups, "")
+			assert.Lenf(policyRule.Resources, 4, "Expected four Resources entry for ClusterRole %s and found %q", clusterRole.Name, policyRule.Resources)
+			assert.Contains(policyRule.Resources, "pods")
+			assert.Contains(policyRule.Resources, "namespaces")
+			assert.Contains(policyRule.Resources, "services")
+			assert.Contains(policyRule.Resources, "daemonsets")
+			assert.Lenf(policyRule.Verbs, 3, "Expected three Verbs entry for ClusterRole %s and found %q", clusterRole.Name, policyRule.Resources)
+			assert.Contains(policyRule.Verbs, "get")
+			assert.Contains(policyRule.Verbs, "watch")
+			assert.Contains(policyRule.Verbs, "list")
+			assert.Lenf(policyRule.ResourceNames, 0, "Expected no ResourceNames entry for ClusterRole %s and found %q", clusterRole.Name, policyRule.ResourceNames)
+			assert.Lenf(policyRule.NonResourceURLs, 0, "Expected no NonResourceURLs entry for ClusterRole %s and found %q", clusterRole.Name, policyRule.NonResourceURLs)
 		}
 	}
-	assert.Truef(t, fileBeatFound, "Expected to get a ClusterRole response for %s", constants.FilebeatName)
-	assert.Truef(t, journalBeatFound, "Expected to get a ClusterRole response for %s", constants.JournalbeatName)
-	assert.Truef(t, nodeExporterFound, "Expected to get a ClusterRole response for %s", constants.NodeExporterName)
-	assert.Falsef(t, unexpectedNamesFound, "Unexpected ClusterRoles returned: %q", unexpectedNames)
+	assert.Truef(fileBeatFound, "Expected to get a ClusterRole response for %s", constants.FilebeatName)
+	assert.Truef(journalBeatFound, "Expected to get a ClusterRole response for %s", constants.JournalbeatName)
+	assert.Truef(nodeExporterFound, "Expected to get a ClusterRole response for %s", constants.NodeExporterName)
+	assert.Falsef(unexpectedNamesFound, "Unexpected ClusterRoles returned: %q", unexpectedNames)
 }
