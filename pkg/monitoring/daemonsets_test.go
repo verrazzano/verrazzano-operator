0ee740b007ecf4c777111056dd6889319d93ec1e
Paul Mackin <paul.mackin@oracle.com>
2020-09-23 18:59:09 -0400

    checkpoint

--- pkg/monitoring/daemonsets_test.go
+++ pkg/monitoring/daemonsets_test.go
@@ -39,13 +39,13 @@ func TestCreateFilebeatDaemonSet(t *testing.T) {
 }
 
 func validateFilebeatDaemonset(assert *assert.Assertions, v appsv1.DaemonSet, clusterName string) {
-	filebeatLabels := GetFilebeatLabels(clusterName)
+	labels := GetFilebeatLabels(clusterName)
 
 	assert.Equal(constants.LoggingNamespace, v.Namespace)
-	assert.Equal(filebeatLabels, v.Labels)
-	assert.Equal(filebeatLabels, v.Spec.Selector.MatchLabels)
+	assert.Equal(labels, v.Labels)
+	assert.Equal(labels, v.Spec.Selector.MatchLabels)
 	assert.Equal("RollingUpdate", string(v.Spec.UpdateStrategy.Type))
-	assert.Equal(filebeatLabels, v.Spec.Template.Labels)
+	assert.Equal(labels, v.Spec.Template.Labels)
 
 	// Volumes
 	assert.Lenf(v.Spec.Template.Spec.Volumes,4, "Spec.Template.Spec.Volumes has wrong number of items")
@@ -132,8 +132,122 @@ func validateFilebeatDaemonset(assert *assert.Assertions, v appsv1.DaemonSet, cl
 	assert.True(v.Spec.Template.Spec.Containers[0].VolumeMounts[3].ReadOnly)
 	assert.Equal("/var/lib/docker/containers", v.Spec.Template.Spec.Containers[0].VolumeMounts[3].MountPath)
 
+	assert.Equal(corev1.PullIfNotPresent, v.Spec.Template.Spec.Containers[0].ImagePullPolicy)
 
+	assert.Nil(v.Spec.Template.Spec.Containers[0].SecurityContext.Privileged)
+	assert.Zero(*v.Spec.Template.Spec.Containers[0].SecurityContext.RunAsUser)
 }
+
+func validateJournalbeatDaemonset(assert *assert.Assertions, v appsv1.DaemonSet, clusterName string) {
+	labels := GetFilebeatLabels(clusterName)
+
+	assert.Equal(constants.LoggingNamespace, v.Namespace)
+	assert.Equal(labels, v.Labels)
+	assert.Equal(labels, v.Spec.Selector.MatchLabels)
+	assert.Equal("RollingUpdate", string(v.Spec.UpdateStrategy.Type))
+	assert.Equal(labels, v.Spec.Template.Labels)
+
+	// Volumes
+	assert.Lenf(v.Spec.Template.Spec.Volumes,5, "Spec.Template.Spec.Volumes has wrong number of items")
+
+	assert.Equal("config", v.Spec.Template.Spec.Volumes[0].Name)
+	assert.Equal(v.Name + "-config", v.Spec.Template.Spec.Volumes[0].VolumeSource.ConfigMap.LocalObjectReference.Name)
+	assert.Equal(int32(0600), *v.Spec.Template.Spec.Volumes[0].VolumeSource.ConfigMap.DefaultMode)
+	assert.Equal("journalbeat.yml", v.Spec.Template.Spec.Volumes[0].VolumeSource.ConfigMap.Items[0].Key)
+
+	assert.Equal("var-log-journal", v.Spec.Template.Spec.Volumes[1].Name)
+	assert.Equal("/var/log/journal", v.Spec.Template.Spec.Volumes[1].VolumeSource.HostPath.Path)
+
+	assert.Equal("run-log-journal", v.Spec.Template.Spec.Volumes[2].Name)
+	assert.Equal("/run/log/journal", v.Spec.Template.Spec.Volumes[2].VolumeSource.HostPath.Path)
+
+	assert.Equal("etc-machine-id", v.Spec.Template.Spec.Volumes[3].Name)
+	assert.Equal("/etc/machine-id", v.Spec.Template.Spec.Volumes[3].VolumeSource.HostPath.Path)
+
+	assert.Equal("data", v.Spec.Template.Spec.Volumes[4].Name)
+	assert.Equal("/var/lib/journalbeat-data", v.Spec.Template.Spec.Volumes[4].VolumeSource.HostPath.Path)
+	assert.Nil( v.Spec.Template.Spec.Volumes[4].VolumeSource.HostPath.Type)
+
+	// Containers
+	assert.Lenf(v.Spec.Template.Spec.Containers,1, "Spec.Template.Spec.Containers has wrong number of items")
+	assert.Equal(v.Name, v.Spec.Template.Spec.Containers[0].Name)
+	assert.Equal(util.GetFilebeatImage(), v.Spec.Template.Spec.Containers[0].Image)
+	assert.Nil(v.Spec.Template.Spec.Containers[0].Command)
+	assert.Len( v.Spec.Template.Spec.Containers[0].Args,3,
+		"Spec.Template.Spec.Containers[0].Args has the wrong number of args)" )
+	assert.Equal("-c", v.Spec.Template.Spec.Containers[0].Args[0])
+	assert.Equal("/etc/filebeat/journalbeat.yml", v.Spec.Template.Spec.Containers[0].Args[1])
+	assert.Equal("-e", v.Spec.Template.Spec.Containers[0].Args[2])
+	validateResSize(assert, v.Spec.Template.Spec.Containers[0].Resources.Limits, "memory", "800Mi",
+		"Spec.Template.Spec.Containers[0].Resources.Limits")
+	validateResSize(assert, v.Spec.Template.Spec.Containers[0].Resources.Requests, "cpu", "200m",
+		"Spec.Template.Spec.Containers[0].Resources.Limits")
+	validateResSize(assert, v.Spec.Template.Spec.Containers[0].Resources.Requests, "memory", "200Mi",
+		"Spec.Template.Spec.Containers[0].Resources.Limits")
+
+	assert.Empty(v.Spec.Template.Spec.Containers[0].WorkingDir)
+	assert.Nil(v.Spec.Template.Spec.Containers[0].Ports)
+	assert.Nil(v.Spec.Template.Spec.Containers[0].EnvFrom)
+
+	// Env Vars
+	assert.Equal("NODENAME", v.Spec.Template.Spec.Containers[0].Env[0].Name)
+	assert.Equal("v1", v.Spec.Template.Spec.Containers[0].Env[0].ValueFrom.FieldRef.APIVersion)
+	assert.Equal("spec.nodeName", v.Spec.Template.Spec.Containers[0].Env[0].ValueFrom.FieldRef.FieldPath)
+
+	assert.Equal("ES_URL", v.Spec.Template.Spec.Containers[0].Env[1].Name)
+	assert.Equal(fmt.Sprintf("http://vmi-system-es-ingest.%s.svc.cluster.local", constants.VerrazzanoNamespace),
+		v.Spec.Template.Spec.Containers[0].Env[1].Value)
+
+	assert.Equal("ES_USER", v.Spec.Template.Spec.Containers[0].Env[2].Name)
+	assert.Equal(constants.JournalbeatName + "-secret",
+		v.Spec.Template.Spec.Containers[0].Env[2].ValueFrom.SecretKeyRef.LocalObjectReference.Name)
+	assert.Equal("username", v.Spec.Template.Spec.Containers[0].Env[2].ValueFrom.SecretKeyRef.Key)
+
+
+	assert.Equal("ES_PASSWORD", v.Spec.Template.Spec.Containers[0].Env[3].Name)
+	assert.Equal(constants.JournalbeatName + "-secret",
+		v.Spec.Template.Spec.Containers[0].Env[3].ValueFrom.SecretKeyRef.LocalObjectReference.Name)
+	assert.Equal("password", v.Spec.Template.Spec.Containers[0].Env[3].ValueFrom.SecretKeyRef.Key)
+
+	assert.Equal("ES_PORT", v.Spec.Template.Spec.Containers[0].Env[4].Name)
+	assert.Equal("9200", v.Spec.Template.Spec.Containers[0].Env[4].Value)
+
+	assert.Equal("INDEX_NAME", v.Spec.Template.Spec.Containers[0].Env[5].Name)
+	assert.Equal(constants.JournalbeatName + "-index-config",
+		v.Spec.Template.Spec.Containers[0].Env[5].ValueFrom.ConfigMapKeyRef.LocalObjectReference.Name)
+	assert.Equal(constants.JournalbeatName + "-index-name",
+		v.Spec.Template.Spec.Containers[0].Env[5].ValueFrom.ConfigMapKeyRef.Key)
+
+	// Volume mounts
+	assert.Equal("config", v.Spec.Template.Spec.Containers[0].VolumeMounts[0].Name)
+	assert.True(v.Spec.Template.Spec.Containers[0].VolumeMounts[0].ReadOnly)
+	assert.Equal("/etc/journalbeat", v.Spec.Template.Spec.Containers[0].VolumeMounts[0].MountPath)
+
+	assert.Equal("run-log-journal", v.Spec.Template.Spec.Containers[0].VolumeMounts[1].Name)
+	assert.True(v.Spec.Template.Spec.Containers[0].VolumeMounts[1].ReadOnly)
+	assert.Equal("/run/log/journal", v.Spec.Template.Spec.Containers[0].VolumeMounts[1].MountPath)
+
+	assert.Equal("var-log-journal", v.Spec.Template.Spec.Containers[0].VolumeMounts[2].Name)
+	assert.True(v.Spec.Template.Spec.Containers[0].VolumeMounts[2].ReadOnly)
+	assert.Equal("/var/log/journal", v.Spec.Template.Spec.Containers[0].VolumeMounts[2].MountPath)
+
+	assert.Equal("data", v.Spec.Template.Spec.Containers[0].VolumeMounts[3].Name)
+	assert.Equal("/usr/share/journalbeat/data", v.Spec.Template.Spec.Containers[0].VolumeMounts[3].MountPath)
+
+	assert.Equal("etc-machine-id", v.Spec.Template.Spec.Containers[0].VolumeMounts[4].Name)
+	assert.True(v.Spec.Template.Spec.Containers[0].VolumeMounts[4].ReadOnly)
+	assert.Equal("etc-machine-id", v.Spec.Template.Spec.Containers[0].VolumeMounts[4].MountPath)
+
+	assert.Equal(corev1.PullIfNotPresent, v.Spec.Template.Spec.Containers[0].ImagePullPolicy)
+
+	assert.Nil(v.Spec.Template.Spec.Containers[0].SecurityContext.Privileged)
+	assert.Zero(*v.Spec.Template.Spec.Containers[0].SecurityContext.RunAsUser)
+	assert.Nil(v.Spec.Template.Spec.Containers[0].SecurityContext.ProcMount)
+
+	assert.Equal(30, *v.Spec.Template.Spec.TerminationGracePeriodSeconds)
+	assert.Equal(v.Name, v.Spec.Template.Spec.ServiceAccountName)
+}
+
 
 func validateResSize(assert *assert.Assertions, rm corev1.ResourceList, key corev1.ResourceName, val string, field string) {
 	q, ok := rm[key]
