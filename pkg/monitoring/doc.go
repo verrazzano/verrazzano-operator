5a614e973e30e0933aef381a3979662856eb27df
Paul Mackin <paul.mackin@oracle.com>
2020-09-24 12:17:29 -0400

    add unit tests

---
+++ pkg/monitoring/doc.go
@@ -0,0 +1,9 @@
+// Copyright (c) 2020, Oracle and/or its affiliates.
+// Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
+
+package monitoring
+
+/*
+Package moinitoring manages resources used by Filebeats, Journalbeats, and NodeExporters.  Filebeats and Journalbeats ship
+log data to Elasticsearch.  NodeExporters send metrics to prometheus.
+*/
