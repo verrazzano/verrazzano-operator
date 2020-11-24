a1a62218321f747ebc8f2bdd54ab2737656ae8da
Fred Tibbitts <fred.tibbitts@oracle.com>
2020-11-24 08:48:23 -0500

    Fix comment

--- pkg/managed/secrets.go
+++ pkg/managed/secrets.go
@@ -152,7 +152,7 @@ func newSecrets(mbPair *types.ModelBindingPair, managedCluster *types.ManagedClu
 	// Process WebLogic domain secrets
 	for _, domain := range managedCluster.WlsDomainCRs {
 		// Each WebLogic domain requires a runtime encryption secret that contains a randomly generated password.
-		// Note: we are assuming that each domain has DomainHomeSourceType is set to FromModel.
+		// Note: we are assuming that each domain has DomainHomeSourceType set to FromModel.
 
 		// Find the namespace for this domain in the binding placements
 		namespace, err := util.GetComponentNamespace(domain.Name, binding)
