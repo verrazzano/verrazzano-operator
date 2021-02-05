78eedd8bcae47ce7fdafcca6caed97bfffd130c2
Paul Mackin <paul.mackin@oracle.com>
2021-02-05 13:02:21 -0500

    update copyrights

--- pkg/managed/namespace.go
+++ pkg/managed/namespace.go
@@ -1,4 +1,4 @@
-// Copyright (c) 2020, Oracle and/or its affiliates.
+// Copyright (C) 2020, 2021, Oracle and/or its affiliates.
 // Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
 
 // Handles creation/deletion of deployments based, on a VerrazzanoBinding
