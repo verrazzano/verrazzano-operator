06799d8c0a74418087739d78a18c5f0feb6d201b
Paul Mackin <paul.mackin@oracle.com>
2022-02-03 10:54:45 -0500

    use correct timestamp format

--- pkg/util/logs/logs.go
+++ pkg/util/logs/logs.go
@@ -6,8 +6,11 @@ package logs
 import (
 	"go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
+	ctrl "sigs.k8s.io/controller-runtime/pkg/log"
 	kzap "sigs.k8s.io/controller-runtime/pkg/log/zap"
 )
+
+const timeFormat = "2006-01-02T15:04:05.000Z"
 
 // InitLogs initializes logs with Time and Global Level of Logs set at Info
 func InitLogs(opts kzap.Options) {
@@ -22,7 +25,7 @@ func InitLogs(opts kzap.Options) {
 	} else {
 		config.Level.SetLevel(zapcore.InfoLevel)
 	}
-	config.EncoderConfig.EncodeTime = zapcore.RFC3339TimeEncoder
+	config.EncoderConfig.EncodeTime = zapcore.TimeEncoderOfLayout(timeFormat)
 	config.EncoderConfig.TimeKey = "@timestamp"
 	config.EncoderConfig.MessageKey = "message"
 	logger, err := config.Build()
@@ -31,4 +34,19 @@ func InitLogs(opts kzap.Options) {
 	} else {
 		zap.ReplaceGlobals(logger)
 	}
+
+	// Use a zap logr.Logger implementation. If none of the zap
+	// flags are configured (or if the zap flag set is not being
+	// used), this defaults to a production zap logger.
+	//
+	// The logger instantiated here can be changed to any logger
+	// implementing the logr.Logger interface. This logger will
+	// be propagated through the whole operator, generating
+	// uniform and structured logs.
+	//
+	// Add the caller field as an option otherwise the controller runtime logger
+	// will not include the caller field.
+	opts.ZapOpts = append(opts.ZapOpts, zap.AddCaller())
+	encoder := zapcore.NewJSONEncoder(config.EncoderConfig)
+	ctrl.SetLogger(kzap.New(kzap.UseFlagOptions(&opts), kzap.Encoder(encoder)))
 }
